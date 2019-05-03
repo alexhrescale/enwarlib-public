bdd082f886eda8302828e9208c56a149b96c5a54
Alex Huang <alexh@rescale.com>
2019-05-03 16:25:55 -0700

    Update default.nix to python3, auto install deps + update readme

--- default.nix
+++ default.nix
@@ -8,16 +8,28 @@ stdenv.mkDerivation rec {
     };
     buildInputs = [
         git
-        python27Packages.virtualenv
+        # python27Packages.virtualenv
+        python37Packages.virtualenv
     ];
     shellHook = ''
         export SOURCE_DATE_EPOCH=$(date +%s);
-        export PYTHONPATH=.:$PYTHONPATH
+        export PYTHONPATH=.:$PYTHONPATH;
 
-        if [ ! -e venv ]; then
-            virtualenv venv
+        PYTHON_VERSION=$(python -c 'import sys;print(sys.version_info.major)');
+        VIRTUAL_ENV=''${VIRTUAL_ENV-venv$PYTHON_VERSION};
+        if [ ! $VIRTUAL_ENV ]; then
+            echo "using existing virtualenv at $VIRTUAL_ENV..."
+            source $VIRTUAL_ENV/bin/activate
+        else
+            echo "creating virtualenv at $VIRTUAL_ENV..."
+            if [ $PYTHON_VERSION -eq 2 ]; then
+                virtualenv $VIRTUAL_ENV
+            else
+                python -m venv $VIRTUAL_ENV
+            fi
+            source $VIRTUAL_ENV/bin/activate
+            python setup.py install
         fi
-        source venv/bin/activate
         if ! [ -x "$(command -v pip-sync)" ]; then
             echo 'pip-sync not found; installing pip-tools...'
             pip install pip-tools
